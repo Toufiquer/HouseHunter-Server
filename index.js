56ec7004d4873ab9f6a67430ee9c97ff3da0d071
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-07-18 04:26:09 +0600

    done @ mongoose curd

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run() {
     const UserSchema = new Schema(_schema);
     const UserModal = mongoose.model("usercollections", UserSchema);
 
+    // add user
     const addUser = () => {
       const user = new UserModal({
         name: "user",
@@ -29,8 +30,50 @@ async function run() {
       return user;
     };
 
+    // get a user
+    const getAUser = async () => {
+      const lastUser = await UserModal.findOne({ name: "user" });
+      return lastUser;
+    };
+    // get all user
+    const getAllUser = async () => {
+      const lastUser = await UserModal.find({});
+      return lastUser;
+    };
+    // update user
+    const updateUser = async (name, user) => {
+      const filter = { name };
+      const update = { ...user };
+      const updateUser = await UserModal.findOneAndUpdate(filter, update);
+      return updateUser;
+    };
+    // delete user
+    const deleteUser = async (name) => {
+      const filter = { ...name };
+      const updateUser = await UserModal.deleteOne(filter);
+      return updateUser;
+    };
+
     app.get("/add", (req, res) => {
       const user = addUser();
+      res.send({ data: user });
+    });
+
+    app.get("/update", async (req, res) => {
+      const user = await updateUser("user", { name: "user 01" });
+      res.send({ data: user });
+    });
+
+    app.get("/delete", async (req, res) => {
+      const user = await deleteUser({ name: "user 01" });
+      res.send({ data: user });
+    });
+    app.get("/get", async (req, res) => {
+      const user = await getAUser();
+      res.send({ data: user });
+    });
+    app.get("/getall", async (req, res) => {
+      const user = await getAllUser();
       res.send({ data: user });
     });
   } catch (err) {
